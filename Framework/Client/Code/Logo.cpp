0f2ec3b08a01866b96a34aad961bd016b3036ab8
hlino77 <ghkddlsqja77@naver.com>
2023-07-04 15:14:45 +0900

    Fix : 오류 수정

--- Framework/Client/Code/Logo.cpp
+++ Framework/Client/Code/Logo.cpp
@@ -96,9 +96,9 @@ HRESULT CLogo::Ready_Layer_Environment(LAYER_TYPE _eType)
 	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"MainCamera", pCamera), E_FAIL);
 	
 
-	CPushStone* pPush = CPushStone::Create(_vec3(0, 1, 0), m_pGraphicDev);
-	NULL_CHECK_RETURN(pPlayer, E_FAIL);
-	FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Stone", pPush), E_FAIL);
+	//CPushStone* pPush = CPushStone::Create(_vec3(0, 1, 0), m_pGraphicDev);
+	//NULL_CHECK_RETURN(pPlayer, E_FAIL);
+	//FAILED_CHECK_RETURN(pLayer->Add_GameObject(L"Stone", pPush), E_FAIL);
 
 
 	// UI
