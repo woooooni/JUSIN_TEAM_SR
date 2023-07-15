fea2d7147432b75def0c0214537da7abde7b35d3
Tiny-Prince <rlaxodnjs6574@gmail.com>
2023-07-15 18:24:54 +0900

    Feature : "Render Group 추가."

--- Framework/Engine/Utility/Code/Renderer.cpp
+++ Framework/Engine/Utility/Code/Renderer.cpp
@@ -34,6 +34,7 @@ void CRenderer::Render_GameObject(LPDIRECT3DDEVICE9& pGraphicDev)
 	Render_Priority(pGraphicDev);
 	Render_Nonalpha(pGraphicDev);
 	Render_Alpha(pGraphicDev);
+	Render_Effect(pGraphicDev);
 	Render_UI(pGraphicDev);
 
 
@@ -82,6 +83,31 @@ void CRenderer::Render_Alpha(LPDIRECT3DDEVICE9& pGraphicDev)
 
 	pGraphicDev->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
 	pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+}
+
+void CRenderer::Render_Effect(LPDIRECT3DDEVICE9& pGraphicDev)
+{
+	pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, FALSE);
+	pGraphicDev->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+	pGraphicDev->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	pGraphicDev->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+
+	pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
+	pGraphicDev->SetRenderState(D3DRS_ALPHAREF, (DWORD)0x00000001);
+	pGraphicDev->SetRenderState(D3DRS_ALPHAFUNC, D3DCMP_GREATEREQUAL);
+
+	m_RenderGroup[RENDER_EFFECT].sort([](CGameObject* pDst, CGameObject* pSrc)
+		{
+			return pDst->Get_ViewZ() > pSrc->Get_ViewZ();
+		});
+
+	for (auto iter : m_RenderGroup[RENDER_EFFECT])
+		iter->Render_Object();
+
+
+	pGraphicDev->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+	pGraphicDev->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+	pGraphicDev->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);
 }
 
 void CRenderer::Render_UI(LPDIRECT3DDEVICE9& pGraphicDev)
