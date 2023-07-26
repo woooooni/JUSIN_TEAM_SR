f823d50d1a930656affd174bc580d4bf186d092d
WonhyeJwa <jade940424@gmail.com>
2023-07-26 21:46:06 +0900

    Feature : "퀘스트 대화내용 작성중"

--- Framework/Client/Code/Quest_NueHero2.cpp
+++ Framework/Client/Code/Quest_NueHero2.cpp
@@ -3,10 +3,16 @@
 CQuest_NueHero2::CQuest_NueHero2()
 	:CQuest_Conversation(NPC_CODE::NPC_NUE_HERO, nullptr)
 {
+	// 순수 누에용사
 	m_strQuestTitle = L"누에용사와 대화하기.";
 	m_strQuestDesc = L"타락한 누에용사를 혼내줬다. 순수해진 누에용사와 대화해보자.";
 
 	// TODO :: 누에용사 대사 쓰기
+	m_vecNpcDescList[(_uint)QUEST_PROGRESS::BEFORE].push_back(L"");
+
+	m_vecNpcDescList[(_uint)QUEST_PROGRESS::CONTINUE].push_back(L"");
+
+	m_vecNpcDescList[(_uint)QUEST_PROGRESS::COMPLETE].push_back(L"");
 	
 }
 
