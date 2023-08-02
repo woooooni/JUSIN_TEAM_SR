59fcc5cae32a1f10a099efa1eb3b2c99884a5b48
ModernCrusader97 <whdtjr970717@gmail.com>
2023-08-03 04:31:55 +0900

    Feature: 썬골렘 사운드 추가

--- Framework/Client/Code/Scene_SunGolemCave1.cpp
+++ Framework/Client/Code/Scene_SunGolemCave1.cpp
@@ -46,7 +46,7 @@ HRESULT CScene_SunGolemCave1::Ready_Scene()
 	FAILED_CHECK_RETURN(Ready_Layer_UI(), E_FAIL);
 
 	Stop_Sound(CHANNELID::SOUND_BGM);
-	Play_BGM(L"BGM_9_SunGolem.wav", 0.3f);
+	Play_BGM(L"BGM_9_SunGolem.wav", 0.2f);
 
 	D3DLIGHT9 tLight;
 	tLight.Type = D3DLIGHTTYPE::D3DLIGHT_DIRECTIONAL;
